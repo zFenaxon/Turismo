dfc1f34b63cc66f2d22404af343195cae0d1da4b
zFenaxon <fe.sotomayor@duocuc.cl>
2021-12-07 16:43:38 -0300

    modelosa gregados al admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Zona)
+admin.site.register(Vehiculo)
+admin.site.register(Transporte)
+admin.site.register(Tour)
+admin.site.register(Servicios)
+admin.site.register(Reserva)
+admin.site.register(Mantenimiento)
+admin.site.register(Inventario)
+admin.site.register(Departamento)
+admin.site.register(Conductor)
+admin.site.register(CheckIn)
+admin.site.register(CheckOut)
